2789fe989c3c11c130d443369ffb1698fb6d6897
Jeremy Hill <j.c.hillmail@gmail.com>
2019-07-13 03:17:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def monopoly_with_fourth_tier
 	      :one_piece_owned=>25,
 	      :two_pieces_owned=>50,
         :three_pieces_owned=>100,
-        
+        :four_pieces_owned=>200
 	    }
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
